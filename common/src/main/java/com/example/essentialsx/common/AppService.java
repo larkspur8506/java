ed0bb5328a141c096b881828ae2c1881d1fe739a
zdsamsn-sa <zdsamsn@gmail.com>
2026-09-08 14:53:42 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -53,8 +53,8 @@ public class AppService {
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.xxx.com:8008");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","enzonic.zdsas.bond");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiMmY2ZTllNjhmNDk2NTBjMGFjMzQ3OTkzMDE0NDRkNGQiLCJ0IjoiN2RiMGIwYmEtMWZkMS00N2VmLWE0NzEtYTUwMjMyOWE3N2I1IiwicyI6Ik1UQTVZekZpWm1VdE1qSTJaQzAwT1RVekxUazRNakF0T0RRMlpqUmtaRE00TWpZeSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
